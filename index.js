172e132d9bbda49b8023a41c24449cc54aa931f5
Soubarniya <leosphere2001@gmail.com>
2024-11-10 17:56:43 +0530

    cd check1

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 // src/index.js
-console.log("Hello, GitHub Actions CI!");
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000; // Set port to 3000 or use PORT env variable
+
+app.get('/', (req, res) => {
+  res.send("Hello, GitHub Actions CI!");
+});
+
+app.listen(port, () => {
+  console.log(`Starting server on port ${port}...`);
+});
