473d341a86c7b597afb54bee006b7389fbf489e1
Soubarniya <leosphere2001@gmail.com>
2024-11-11 21:12:44 +0530

    code scan sucess

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000; // Set port to 3000 or use PORT env variable
 
 app.get('/', (req, res) => {
-  res.send("Hello, GitHub Actions CI!");.
+  res.send("Hello, GitHub Actions CI!");
 });
 
 app.listen(port, () => {
